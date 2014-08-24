38d78d6656d55c4c755e9baa47456b6d70e357a7
DoctahJones <dbainbridge@outlook.com>
2014-08-24 17:27:38 +0100

    update

---
+++ ImageTaggingUnitTest/ImageTaggingTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageTagging;
+
+namespace ImageTaggingUnitTest
+{
+    [TestClass]
+    public class ImageTaggingTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TagUsageData tag = new TagUsageData();
+            
+        }
+    }
+}
